2287bc9e412ea7d3e2afeaa1d93e3fe81b7198b9
Simon ZENG <zengweiking@outlook.com>
2025-12-23 08:19:08 +0800

    update

--- ddd-go-webapi/cmd/sample_api/main.go
+++ ddd-go-webapi/cmd/sample_api/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/zwloveu/rust-usages/ddd-go-webapi/internal/bootstrap"
 	"github.com/zwloveu/rust-usages/ddd-go-webapi/internal/resource"
@@ -14,6 +17,23 @@ func main() {
 		log.Fatalf("failed to init config：%v", err)
 	}
 
+	defer func() {
+		if err := appState.DB.Close(); err != nil {
+			log.Printf("warning: failed to close db connection pool: %v", err)
+		} else {
+			log.Println("db connection pool closed gracefully")
+		}
+	}()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sigChan
+		log.Println("received exit signal, shutting down server...")
+		os.Exit(0) // trigger the exit of main then execute defer statements
+	}()
+
+	log.Println("starting API server...")
 	if err := bootstrap.RunAPI(appState); err != nil {
 		log.Fatalf("Failed to start API：%v", err)
 	}
--- ddd-go-webapi/internal/resource/db.go
+++ ddd-go-webapi/internal/resource/db.go
@@ -16,5 +16,15 @@ func NewDB(cfg *Config) (*sql.DB, error) {
 		return nil, err
 	}
 
+	if !cfg.Common.UseMemoryData {
+		if err := db.Ping(); err != nil {
+			closeErr := db.Close()
+			if closeErr != nil {
+				fmt.Printf("warning: failed to close invalid db pool: %v\n", closeErr)
+			}
+			return nil, fmt.Errorf("failed to ping database: %w", err)
+		}
+	}
+
 	return db, nil
 }
