d46f95630f1972bf428206eed62594024d75f52d
simon <simon.zeng@zw.com>
2025-12-25 23:22:56 +0800

    update

--- ddd-rust/src/lib.rs
+++ ddd-rust/src/lib.rs
@@ -2,15 +2,15 @@ use std::pin::Pin;
 use std::thread;
 use std::{error::Error, future::Future};
 
+use tokio::task::JoinSet;
 use tokio::time::Instant;
 
-pub async fn run<F, E>(f: F) -> Result<(), Box<dyn Error>>
+pub async fn run<F, E>(f: F) -> Result<(), Box<dyn Error + Send + Sync>>
 where
-    F: Future<Output = Result<(), E>>,
-    E: Into<Box<dyn Error>>,
+    F: Future<Output = Result<(), E>> + Send,
+    E: Into<Box<dyn Error + Send + Sync>>,
 {
     f.await.map_err(|e| e.into())?;
-
     Ok(())
 }
 
@@ -18,31 +18,35 @@ where
 //     Err("something went wrong".into())
 // }
 
-pub async fn ddd_rust_entry() -> Result<(), Box<dyn std::error::Error>> {
+pub async fn ddd_rust_entry() -> Result<(), Box<dyn Error + Send + Sync>> {
     let start = Instant::now();
-    let tasks: Vec<_> = (0..=100000)
-        .map(|i| async move {
-            tokio::time::sleep(tokio::time::Duration::from_millis(1000)).await;
 
+    let mut set = JoinSet::new();
+
+    for i in 0..=100_000 {
+        set.spawn(async move {
+            tokio::time::sleep(tokio::time::Duration::from_millis(1000)).await;
             if i % 10000 == 0 {
                 println!(
-                    "Task {} done | thread id: {:?} | elapsed: {}ms",
+                    "[{}ms] | Task {} done | thread: {:?}",
+                    start.elapsed().as_millis(),
                     i,
-                    thread::current().id(),
-                    start.elapsed().as_millis()
+                    std::thread::current().id()
                 );
             }
-        })
-        .collect();
+        });
+    }
 
-    futures::future::join_all(tasks).await;
+    // Wait for all tasks in the set to finish
+    while let Some(_) = set.join_next().await {}
+
     println!(
         "[{}ms] All tasks done, thread id: {:?}",
         start.elapsed().as_millis(),
         thread::current().id()
     );
 
-    let async_add_futures: Vec<Pin<Box<dyn Future<Output = i32>>>> =
+    let async_add_futures: Vec<Pin<Box<dyn Future<Output = i32> + Send>>> =
         vec![Box::pin(async_add(1, 2)), Box::pin(get_async_add_future())];
     let results: Vec<i32> = futures::future::join_all(async_add_futures).await;
     println!("[{}ms] {:?}", start.elapsed().as_millis(), results);
@@ -55,6 +59,6 @@ async fn async_add(a: i32, b: i32) -> i32 {
     a + b
 }
 
-fn get_async_add_future() -> impl Future<Output = i32> {
+fn get_async_add_future() -> impl Future<Output = i32> + Send {
     async_add(1, 2)
 }
--- ddd-rust/src/main.rs
+++ ddd-rust/src/main.rs
@@ -2,12 +2,14 @@ use ddd_rust::{ddd_rust_entry, run};
 use std::error::Error;
 use tokio::runtime::Runtime;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     let runtime: Runtime = tokio::runtime::Builder::new_multi_thread()
         .enable_all()
         .build()?;
 
-    let result = runtime.block_on(async { run(ddd_rust_entry()).await });
+    let server_handle = runtime.spawn(run(ddd_rust_entry()));
+
+    let result = runtime.block_on(server_handle)?;
 
     if let Err(e) = result {
         eprintln!("Application error: {}", e);
