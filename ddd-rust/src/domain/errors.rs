4ed365bc60abd09dfc807ed94030b85d2d2de3fe
Simon ZENG <zengweiking@outlook.com>
2026-01-03 03:33:01 +0800

    update

--- ddd-rust/src/domain/errors.rs
+++ ddd-rust/src/domain/errors.rs
@@ -2,21 +2,51 @@
 #[error("Fatal system error: {0}")]
 pub struct FatalError(pub String);
 
+impl From<&str> for FatalError {
+    fn from(value: &str) -> Self {
+        Self(value.to_string())
+    }
+}
+
+impl From<String> for FatalError {
+    fn from(value: String) -> Self {
+        Self(value)
+    }
+}
+
 #[derive(thiserror::Error, Debug, Clone)]
 #[error("Recoverable task error: {0}")]
 pub struct RecoverableError(pub String);
 
+impl From<&str> for RecoverableError {
+    fn from(value: &str) -> Self {
+        Self(value.to_string())
+    }
+}
+
+impl From<String> for RecoverableError {
+    fn from(value: String) -> Self {
+        Self(value)
+    }
+}
+
 #[derive(thiserror::Error, Debug)]
 pub enum AppError {
     #[error("Fatal error: {error}")]
-    Fatal {
-        #[from]
-        error: FatalError,
-    },
+    Fatal { error: FatalError },
 
     #[error("Recoverable error: {error}")]
-    Recoverable {
-        #[from]
-        error: RecoverableError,
-    },
+    Recoverable { error: RecoverableError },
+}
+
+impl From<FatalError> for AppError {
+    fn from(error: FatalError) -> Self {
+        Self::Fatal { error }
+    }
+}
+
+impl From<RecoverableError> for AppError {
+    fn from(error: RecoverableError) -> Self {
+        Self::Recoverable { error }
+    }
 }
