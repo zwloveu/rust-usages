75e26a7826882ad196ba460d5f474ac195bc347f
Simon ZENG <zengweiking@outlook.com>
2026-01-07 08:18:31 +0800

    update

--- ddd-rust/src/domain.rs
+++ ddd-rust/src/domain.rs
@@ -4,12 +4,43 @@ pub mod events;
 
 use std::{pin::Pin, sync::Arc};
 
+use crossbeam_channel::Sender;
 use tokio_util::sync::CancellationToken;
 
-pub type TaskResult = Result<(), errors::AppError>;
+pub type TaskResult = TaskResultGeneric<()>;
 pub type BoxedFuture = Pin<Box<dyn Future<Output = TaskResult> + Send>>;
 pub type TaskFactory = Box<dyn Fn(CancellationToken) -> BoxedFuture + Send>;
 pub struct TaskDefinition {
     pub name: Arc<str>,
     pub factory: TaskFactory,
+}
+
+pub type TaskResultGeneric<T> = Result<T, errors::AppError>;
+
+pub fn build_task<F, Fut, C, R>(
+    f: F,
+    ct: CancellationToken,
+    tx: Sender<C>,
+) -> impl Future<Output = TaskResultGeneric<R>> + Send + 'static
+where
+    F: FnOnce(CancellationToken, Sender<C>) -> Fut,
+    Fut: Future<Output = TaskResultGeneric<R>> + Send + 'static,
+{
+    f(ct, tx)
+}
+
+pub fn build_task_with_name<F, Fut, C, R>(
+    name: Arc<str>,
+    f: F,
+    ct: CancellationToken,
+    tx: Sender<C>,
+) -> (
+    Arc<str>,
+    impl Future<Output = TaskResultGeneric<R>> + Send + 'static,
+)
+where
+    F: FnOnce(CancellationToken, Sender<C>) -> Fut,
+    Fut: Future<Output = TaskResultGeneric<R>> + Send + 'static,
+{
+    (name, build_task(f, ct, tx))
 }
